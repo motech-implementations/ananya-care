dad24e578c952030b1ad79f90251aee40280eeff
anuranjan <iit.anuranjan@gmail.com>
2014-08-25 18:47:44 +0530

    Creating a new bundle for common entities and changing version of the modules to 0.24-SNAPSHOT

---
+++ care-reporting/common-entites/src/main/java/org/motechproject/care/common/utils/FormToString.java
@@ -0,0 +1,8 @@
+package org.motechproject.care.common.utils;
+
+public class FormToString {
+    public static String toString(Object object) {
+        Object instanceId = ReflectionUtils.getValue(object, "instanceId");
+        return String.format("%s{instanceId=%s}", object.getClass().getSimpleName(), instanceId);
+    }
+}
